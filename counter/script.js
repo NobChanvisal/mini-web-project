7749c8d05cdc153ef7fdb5ef9c9481ad854d9c2a
NobChanvisal <chanvisal.nob09@gmail.com>
2024-04-03 00:05:44 +0700

    Day 2

---
+++ counter/script.js
@@ -0,0 +1,19 @@
+const DisplayNum = document.querySelector(".display-number");
+const DecreaseButton = document.querySelector(".decrease-button");
+const IncreaseButton = document.querySelector(".increase-button");
+const ResetButton = document.querySelector(".reset-button");
+
+var num = 0;
+DecreaseButton.addEventListener("click",()=>{
+    num += 1;
+    console.log(num);
+    DisplayNum.innerHTML = num;
+})
+IncreaseButton.addEventListener("click",()=>{
+    num -=1;
+    DisplayNum.innerHTML = num;
+})
+ResetButton.addEventListener("click",()=>{
+    num = 0;
+    DisplayNum.innerHTML = num;
+})
