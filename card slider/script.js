e26d3823e64570dfcb416cf4dbeab3e79ce057fd
NobChanvisal <chanvisal.nob09@gmail.com>
2024-04-05 15:26:29 +0700

    Day 4

--- card slider/script.js
+++ card slider/script.js
@@ -1,73 +1,84 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const container = document.querySelector(".container"); 
-    const arrowBtns = document.querySelectorAll(".wrapper i"); 
-    const wrapper = document.querySelector(".wrapper"); 
-  
-    const firstCard = container.querySelector(".card"); 
-    const firstCardWidth = firstCard.offsetWidth; 
-  
-    let isDragging = false, 
-        startX, 
-        startScrollLeft, 
-        timeoutId; 
-  
-    const dragStart = (e) => {  
-        isDragging = true; 
-        container.classList.add("dragging"); 
-        startX = e.pageX; 
-        startScrollLeft = container.scrollLeft; 
-    }; 
+const wrapper = document.querySelector(".wrapper");
+const container = document.querySelector(".container");
+const firstCardWidth = container.querySelector(".card").offsetWidth;
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const containerChildrens = [...container.children];
 
-    const dragging = (e) => { 
-        if (!isDragging) return; 
-      
-        // Calculate the new scroll position 
-        const newScrollLeft = startScrollLeft - (e.pageX - startX); 
-      
-        // Check if the new scroll position exceeds  
-        // the container boundaries 
-        if (newScrollLeft <= 0 || newScrollLeft >=  
-            container.scrollWidth - container.offsetWidth) { 
-              
-            // If so, prevent further dragging 
-            isDragging = false; 
-            return; 
-        } 
-      
-        // Otherwise, update the scroll position of the container 
-        container.scrollLeft = newScrollLeft; 
-    }; 
-  
-    const dragStop = () => { 
-        isDragging = false;  
-        container.classList.remove("dragging"); 
-    }; 
-    const autoPlay = () => { 
-      
-        // Return if window is smaller than 800 
-        if (window.innerWidth < 800) return;  
-          
-        // Calculate the total width of all cards 
-        const totalCardWidth = container.scrollWidth; 
-          
-        // Calculate the maximum scroll position 
-        const maxScrollLeft = totalCardWidth - container.offsetWidth; 
-          
-        // If the carousel is at the end, stop autoplay 
-        if (container.scrollLeft >= maxScrollLeft) return; 
-          
-        // Autoplay the carousel after every 2500ms 
-        timeoutId = setTimeout(() =>  
-            container.scrollLeft += firstCardWidth, 2500); 
-    };
-    container.addEventListener("mousedown", dragStart); 
-    container.addEventListener("mousemove", dragging); 
-    document.addEventListener("mouseup", dragStop); 
-    arrowBtns.forEach(btn => { 
-        btn.addEventListener("click", () => { 
-            container.scrollLeft += btn.id === "left" ?  
-                -firstCardWidth : firstCardWidth; 
-        }); 
-    }); 
-    
+let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+
+// Get the number of cards that can fit in the container at once
+let cardPerView = Math.round(container.offsetWidth / firstCardWidth);
+
+// Insert copies of the last few cards to beginning of container for infinite scrolling
+containerChildrens.slice(-cardPerView).reverse().forEach(card => {
+    container.insertAdjacentHTML("afterbegin", card.outerHTML);
 });
+
+// Insert copies of the first few cards to end of container for infinite scrolling
+containerChildrens.slice(0, cardPerView).forEach(card => {
+    container.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+// Scroll the container at appropriate postition to hide first few duplicate cards on Firefox
+container.classList.add("no-transition");
+container.scrollLeft = container.offsetWidth;
+container.classList.remove("no-transition");
+
+// Add event listeners for the arrow buttons to scroll the container left and right
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        container.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+    });
+});
+
+const dragStart = (e) => {
+    isDragging = true;
+    container.classList.add("dragging");
+    // Records the initial cursor and scroll position of the container
+    startX = e.pageX;
+    startScrollLeft = container.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragging) return; // if isDragging is false return from here
+    // Updates the scroll position of the container based on the cursor movement
+    container.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    container.classList.remove("dragging");
+}
+
+const infiniteScroll = () => {
+    // If the container is at the beginning, scroll to the end
+    if(container.scrollLeft === 0) {
+        container.classList.add("no-transition");
+        container.scrollLeft = container.scrollWidth - (2 * container.offsetWidth);
+        container.classList.remove("no-transition");
+    }
+    // If the container is at the end, scroll to the beginning
+    else if(Math.ceil(container.scrollLeft) === container.scrollWidth - container.offsetWidth) {
+        container.classList.add("no-transition");
+        container.scrollLeft = container.offsetWidth;
+        container.classList.remove("no-transition");
+    }
+
+    // Clear existing timeout & start autoplay if mouse is not hovering over container
+    clearTimeout(timeoutId);
+    if(!wrapper.matches(":hover")) autoPlay();
+}
+
+const autoPlay = () => {
+    if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+    // Autoplay the container after every 2500 ms
+    timeoutId = setTimeout(() => container.scrollLeft += firstCardWidth, 2500);
+}
+autoPlay();
+
+container.addEventListener("mousedown", dragStart);
+container.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+container.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
