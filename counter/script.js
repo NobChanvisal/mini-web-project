64bf1d835028f425610eb5076280878d3ac3d10b
NobChanvisal <chanvisal.nob09@gmail.com>
2025-06-10 14:25:22 +0700

    update

--- counter/script.js
+++ counter/script.js
@@ -5,12 +5,12 @@ const ResetButton = document.querySelector(".reset-button");
 
 var num = 0;
 DecreaseButton.addEventListener("click",()=>{
-    num += 1;
+    num -= 1;
     console.log(num);
     DisplayNum.innerHTML = num;
 })
 IncreaseButton.addEventListener("click",()=>{
-    num -=1;
+    num +=1;
     DisplayNum.innerHTML = num;
 })
 ResetButton.addEventListener("click",()=>{
